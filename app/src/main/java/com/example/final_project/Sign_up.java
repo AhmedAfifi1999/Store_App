e73389b0f3cee5a1bda35fee07e40a2563653594
AhmedAfifi1999 <ahmedafifi1500@gmail.com>
2021-04-02 20:08:46 +0300

    design sign up part two >

--- app/src/main/java/com/example/final_project/Sign_up.java
+++ app/src/main/java/com/example/final_project/Sign_up.java
@@ -2,13 +2,55 @@ package com.example.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Sign_up extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class Sign_up extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    TextView tvDate;
+    Button btPickDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        tvDate = findViewById(R.id.tvDate);
+        btPickDate = findViewById(R.id.btPickDate);
+        tvDate = findViewById(R.id.tvDate);
+        btPickDate = findViewById(R.id.btPickDate);
+        btPickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Please note that use your package name here
+                com.example.final_project.DatePicker mDatePickerDialogFragment;
+                mDatePickerDialogFragment = new com.example.final_project.DatePicker();
+                mDatePickerDialogFragment.show(getSupportFragmentManager(), "DATE PICK");
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public void onDateSet(android.widget.DatePicker view, int year, int month, int dayOfMonth) {
+        // Create a Calender instance
+        Calendar mCalender = Calendar.getInstance();
+        // Set static variables of Calender instance
+        mCalender.set(Calendar.YEAR,year);
+        mCalender.set(Calendar.MONTH,month);
+        mCalender.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        // Get the date in form of string
+        String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalender.getTime());
+        // Set the textview to the selectedDate String
+        tvDate.setText(selectedDate);
     }
 }
